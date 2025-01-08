bbd0718dc374d93560a5822870999c90faa50e59
georgeliamcampbell <georgeliamcampbell94@gmail.com>
2025-01-08 09:46:42 +0000

    Initial server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json())
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log("Readabilify running on port:", port);
+});
+
+// Example endpoint returning version info
+server.get("/v1/versions", (request, response, next) => {
+    response.json({
+        "readabilify": "1.0.0",
+        "readability": "0.5.0",
+        "express": "4.21.2"
+    });
+});
