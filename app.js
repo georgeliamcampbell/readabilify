aa2c7f5a22106e348e9968395f31faf92ffe3c97
georgeliamcampbell <georgeliamcampbell94@gmail.com>
2025-01-09 11:25:26 +0000

    Implement functionality to fetch from URL and process with readability

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const readability = require('@mozilla/readability')
+const readability = require('@mozilla/readability');
+const { Readability } = readability;
 
 
 const server = express();
-server.use(express.json())
+server.use(express.json());
 
 const port = process.env.PORT || 49152;
 
@@ -18,21 +19,15 @@ server.listen(port, () => {
 // Return version info
 server.get("/v1/versions", (request, response, next) => {
     response.json({
-        "readabilify": "1.0.0",
         "readability": "0.5.0",
         "express": "4.21.2"
     });
 });
 
 server.post("/v1/fetch", (request, response, next) => {
-    var request_body = request.body;
-    console.log(request.url);
-    //dom = new JSDOM.fromURL("https://example.com", options).then(dom => {
-    //    console.log(dom.serialize());
-    //});
-
-
-    response.json({
-        "message": "It works!"
+    JSDOM.fromURL(request.body.url).then(dom => {
+        var reader = new Readability(dom.window.document);
+        var response_body = reader.parse();
+        response.json(response_body);
     });
 });
